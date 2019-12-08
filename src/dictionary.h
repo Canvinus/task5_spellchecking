b79f8e20e3dcd09bc11e263b686d4817188ece56
Andreyko <agruzdev1@gmail.com>
2019-12-08 13:19:53 +0300

    Done

--- src/dictionary.h
+++ src/dictionary.h
@@ -7,6 +7,7 @@
 #include  <algorithm>
 #include  <string>
 #include  <stdlib.h>
+#include <fstream>
 
 #include  "hashset.h"
 #include  "hashset.cpp"
@@ -18,9 +19,14 @@ class hash_function
 public:
 
     unsigned int operator()( const string& s )  const {
-        
-        
-        // Complete definition
+
+        unsigned int hash = 0;
+        for (int i = 0; i < s.length(); ++i)
+        {
+            hash += s[i];
+            hash -= (hash << 13) | (hash >> 19);
+        }
+        return hash;
         
     }
 };
@@ -37,9 +43,23 @@ public:
 
 class Dictionary: public HashSet<string, hash_function, equality> {
 
-    // Complete definition
 public:
-	Dictionary(string filename);
+	Dictionary(string filename)
+	{
+        ifstream lst(filename);
+
+        if (!lst.good())
+            throw invalid_argument("Error occured while opening " + filename);
+
+        string newWord;
+        while (lst.good())
+        {
+            lst >> newWord;
+            this->insert(newWord);
+        }
+
+        lst.close();
+    };
 
 };
 #endif // _DICTIONARY_H_
--- src/main.cpp
+++ src/main.cpp
@@ -16,7 +16,39 @@ void lower(string& s);
 string stripPunct(const string& s);
 void checkSpelling(ifstream& in, Dictionary& dict);
 
+string swap(const string& word, int ai, int bi)
+{
+    string newStr = word;
+    newStr[ai] = word[bi];
+    newStr[bi] = word[ai];
 
+    return newStr;
+}
+
+string remove(const string& word, int index)
+{
+    string newStr;
+    for (int i = 0; i < word.length(); ++i)
+    {
+        if (i != index)
+            newStr += word[i];
+    }
+
+    return newStr;
+}
+
+string insertAt(const string& word, char letter, int index)
+{
+    string newStr;
+    for (int i = 0; i < index; ++i)
+        newStr += word[i];
+
+    newStr += letter;
+    for (int i = index; i < word.length(); ++i)
+        newStr += word[i];
+
+    return newStr;
+}
 
 // program arguments to run, example: main.exe ../../res/wordlist.txt ../../res/test.txt
 int main(int argc, char* argv[]) {
@@ -64,7 +96,56 @@ void checkSpelling(ifstream& in, Dictionary& dict) {
 		string word;
 		while (ss >> word) 
         {
-            // TODO: Complete the spell check of each word
+            // Remove punctuation and make all words lowercase
+            word = stripPunct(word);
+            lower(word);
+
+            // Searching for suggestions
+            if (!dict.search(word))
+            {
+                cout << "The word '" << word << "' at the line " << line_number << " may be wrong.\nSuggestions:\n";
+
+                set<string> suggestions;
+
+                // Transposing of adjacent letters
+                for (int i = 0; i < word.length() - 1; ++i)
+                {
+                    string newWord = swap(word, i, i + 1);
+                    if (dict.search(newWord))
+                        suggestions.insert(newWord);
+                }
+
+                // Removing each letter
+                for (int i = 0; i < word.length(); ++i)
+                {
+                    string newWord = remove(word, i);
+                    if (dict.search(newWord))
+                        suggestions.insert(newWord);
+                }
+
+                // Replacement of all letters
+                for (int i = 0; i < word.length(); ++i)
+                    for (char j = 'a'; j <= 'z'; ++j)
+                    {
+                        string newWord = word;
+                        newWord[i] = j;
+                        if (dict.search(newWord))
+                            suggestions.insert(newWord);
+                    }
+
+                // Inserting letters at any position in a word
+                for (int i = 0; i <= word.length(); ++i)
+                    for (char letter = 'a'; letter <= 'z'; letter++)
+                    {
+                        string newWord = insertAt(word, letter, i);
+                        if (dict.search(newWord))
+                            suggestions.insert(newWord);
+                    }
+
+                // Printing suggestions
+                for (set<string>::iterator it = suggestions.begin(); it != suggestions.end(); ++it)
+                    cout << "\t\t" << *it << "\n";
+            }
 		}
 	}
 }
